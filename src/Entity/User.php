9d0fe63d29ccd0a5649c7d32260c4e0ce0c128c5
Sh-Daria <shmygareva_daria@mail.ru>
2023-11-20 20:02:43 +0700

    Преобразование article в строчку (string)

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -137,4 +137,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function __toString(): string
+    {
+        return $this->getUserIdentifier();
+    }
 }
