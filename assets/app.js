5618646bc9a57c6f7b19770f8140e16eb9992fed
Sh-Daria <shmygareva_daria@mail.ru>
2023-12-04 16:50:34 +0700

    Стилизация bootstrap_5

--- assets/app.js
+++ assets/app.js
@@ -8,3 +8,8 @@ import './bootstrap.js';
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
